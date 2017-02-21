7b8411fe95bc169aabbd354889b346c9da2509e9
karim103 <karim103@umn.edu>
2017-02-21 15:39:24 -0600

    Update Contact.java

--- Contact.java
+++ Contact.java
@@ -1,40 +1,40 @@
 public class Contact{
-  private String name;
-  private long phone;
-  private String address;
-  private String comments;
+        private String name;
+        private long phone;
+        private String address;
+        private String comments;
+
+        public Contact(String name, long phone, String address, String comments){
+                this.name = name;
+                this.phone = phone;
+                this.address = address;
+                this.comments = comments;
+        }
   
-  public Contact(String name, long phone, String address, String comments){
-    this.name = name;
-    this.phone = phone;
-    this.address = address;
-    this.comments = comments;
-  }
-  
-  public String setName(name){
-     this.name = name;
-  }
-  public void getName(){
-    return name;
-  }
-  public long setPhone(phone){
-    this.phone = phone;
-  }
-  public void getPhone(){
-    return phone;
-  }
-  public String setAddress(address){
-    this.address = address;
-  }
-  public void getAddress(){
-    return address;
-  }
-  public String setComments(comments){
-    this.comments = comments;
-  }
-  public void getComments(){
-    return comments;
-  }
-  
-  public String toString(){
-    return "(" + name
+        public String setName(newName){
+                this.name = name;
+        }
+        public void getName(){
+                return newName;
+        }
+        public long setPhone(newPhone){
+                this.phone = phone;
+        }
+        public void getPhone(){
+                return newPhone;
+        }
+        public String setAddress(newAddress){
+                this.address = newAddress;
+        }
+        public void getAddress(){
+                return address;
+        }
+        public String setComments(newComments){
+                this.comments = newComments;
+        }
+        public void getComments(){
+                return comments;
+        }
+
+        public String toString(){
+                return "(" + name
